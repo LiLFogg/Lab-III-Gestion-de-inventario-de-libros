e09c78cb7d71d89ce913b0233493210617748055
Andres Santiago Schneider <andresschneiderr@gmail.com>
2024-06-15 00:04:40 -0300

    inicializacion del proyecto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
+  <div class="nav-bar"><img alt="Vue logo" src="../assets/logo.png" /></div>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,3 +16,18 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.nav-bar {
+  background: linear-gradient(-90deg, #e94817, #d1e704);
+  height: 60px;
+  margin-bottom: 25px;
+  -webkit-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  -moz-box-shadow: 0px 2px 15px -12px rgba(0, 0, 0, 0.57);
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.57);
+  text-align: center;
+  font-size: larger;
+  font-family: "Lucida Sans", "Lucida Sans Regular";
+  font-style: oblique;
+}
+</style>
